5c444cbccc14e937e3ab065a632ccad074f42a5c
XZot1K <alexosborne54@gmail.com>
2018-11-29 08:15:18 -0500

    * Updated to version 1.7.6.7 source files.
    (Configurable Messages & Command Permission)

--- src/XZot1K/plugins/ptg/core/PhysicsToGoCommand.java
+++ src/XZot1K/plugins/ptg/core/PhysicsToGoCommand.java
@@ -8,38 +8,51 @@ import XZot1K.plugins.ptg.PhysicsToGo;
 public class PhysicsToGoCommand implements CommandExecutor
 {
 
-	private final PhysicsToGo plugin;
+    private final PhysicsToGo plugin;
 
-	public PhysicsToGoCommand(PhysicsToGo plugin)
-	{
-		this.plugin = plugin;
-	}
+    public PhysicsToGoCommand(PhysicsToGo plugin)
+    {
+        this.plugin = plugin;
+    }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-	{
-		if (cmd.getName().equalsIgnoreCase("ptg"))
-		{
-			if (args.length == 1)
-			{
-				if (args[0].equalsIgnoreCase("reload"))
-				{
-					plugin.reloadConfig();
-					sender.sendMessage(
-							ChatColor.translateAlternateColorCodes('&', "&aPhysicsToGo successfully " + "reloaded!"));
-					return true;
-				} else
-				{
-					sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cUsage: /ptg <reload>"));
-					return true;
-				}
-			} else
-			{
-				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cUsage: /ptg <reload>"));
-				return true;
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if (cmd.getName().equalsIgnoreCase("ptg"))
+        {
+            if (args.length == 1)
+            {
+                if (args[0].equalsIgnoreCase("reload"))
+                {
+                    if (sender.hasPermission("ptg.reload"))
+                    {
+                        plugin.reloadConfig();
+                        String reloadMessage = plugin.getConfig().getString("messages.reloaded");
+                        if(reloadMessage != null && !reloadMessage.equalsIgnoreCase(""))
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', reloadMessage));
+                    } else
+                    {
+                        String noPermissionMessage = plugin.getConfig().getString("messages.no-permission");
+                        if(noPermissionMessage != null && !noPermissionMessage.equalsIgnoreCase(""))
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermissionMessage));
+                    }
+                    return true;
+                } else
+                {
+                    String usageMessage = plugin.getConfig().getString("messages.usage");
+                    if(usageMessage != null && !usageMessage.equalsIgnoreCase(""))
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', usageMessage));
+                    return true;
+                }
+            } else
+            {
+                String usageMessage = plugin.getConfig().getString("messages.usage");
+                if(usageMessage != null && !usageMessage.equalsIgnoreCase(""))
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', usageMessage));
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
